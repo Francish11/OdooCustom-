8c046a64157dac1cd25a30e20f65571d3dc9d9e2
Francish11 <santosfrancis3@gmail.com>
2024-10-04 20:01:49 -0400

    creacion de modelo para proyecto

--- models/models.py
+++ models/models.py
@@ -17,3 +17,35 @@
 #         for record in self:
 #             record.value2 = float(record.value) / 100
 
+from odoo import models, fields, api
+
+class departamento (models.model):
+    _name = "proyectos.departamento"
+    _descripcion ="Define los atributos de un departamento"
+    
+    #Atributos
+    nombreDpto = fields.char(string="nombre departamento", required=True)
+    
+class empleado(models.model):
+    _name = "proyecto.empleado" 
+    _descripcion = "Define los atributos de un empleado"
+    
+    #Atributos
+    dniempleado = fields.char(string="DNI", Required=True)
+    nombreEmpleado = fields.char(string= "nombre y apellido", required=True)
+    fechaNacimiento = fields.Date(string= "Fecha Nacimiento", required=True, default = fields.date.today())
+    direccionEmpleado = fields.char(string= "Direccion")
+    telefonoEmpleado = fields.char(string= "Telefono")
+    
+class Proyecto(models.model):
+    _name = "proyectos.proyecto"
+    _descripcion = "Define los atributos del proyecto"
+    
+    #Atributos
+    nombreProyecto = fields.char(string= "Nombre del Proyecto", required=True)
+    tipoProyecto = fields.Selection(
+        string= "Tipo de Proyecto", 
+        selection = [('f' , 'Front-End'),('b','Back-End')],
+        help="Tipo de Proyecto al que se esta destinando ")
+
+    
